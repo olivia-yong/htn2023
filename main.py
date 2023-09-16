17c7db8e7f99d4b7643cc01b320c9be7e5d57953
nettlespike <riverthatflowsfast@gmail.com>
2023-09-16 17:32:02 -0400

    No apples, no added length, scoreboard works!!

--- main.py
+++ main.py
@@ -12,27 +12,26 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 
 #game screen
-gamedis_width = 800
+gamedis_width = 600
 gamedis_height = 600
 size = [gamedis_width, gamedis_height]
 
 #scoreboard
-scoreboard_width= 200
+scoreboard_width = 200
 scoreboard_height = 600
 
 score_font_type = pygame.font.SysFont(None, 36)
 
 #snake
 snake_head_img = "./ASSETS/snake_head_test.png"
-pos_x = gamedis_width / 2 + scoreboard_width / 2
+pos_x = gamedis_width / 2
 pos_y = gamedis_height / 2
 pos_x_change = 0
 pos_y_change = 0
 
 
 #apples
-game_score = 0 
-
+game_score = 0
 game_over = False
 
 #speed
@@ -41,29 +40,30 @@ snake_speed = 30
 
 ######################################################
 #MAKE GAME SCREEN
-gamedis = pygame.display.set_mode(size)
+gamedis = pygame.display.set_mode((800, 600))
+
+#make main playing area
+game_area = pygame.Rect(200, 0, gamedis_width, gamedis_height)
+pygame.draw.rect(gamedis, black, game_area)
 
 #make scoreboard
-pygame.draw.rect(gamedis, orange, pygame.Rect(0, 0, scoreboard_width, scoreboard_height))
-#scoreboard_surface = pygame.Surface((scoreboard_width, scoreboard_height))
-#scoreboard_surface.fill(orange)
-score_display = score_font_type.render(f'Score: {game_score}', True, orange)
+scoreboard = pygame.Rect(0, 0, scoreboard_width, scoreboard_height)
+pygame.draw.rect(gamedis, orange, scoreboard)
+score_display = score_font_type.render(f'Score: {game_score}', True, black)
 gamedis.blit(score_display, (30, 100))
-pygame.display.update()
 
 #make initial snake
 snake_head = pygame.image.load(snake_head_img).convert()
 snake_head = pygame.transform.scale(snake_head, (10, 10))
 gamedis.blit(snake_head, (pos_x, pos_y))
 
-pygame.display.update()
+pygame.display.flip()
 pygame.display.set_caption("Snakes eat rocks")
 
 
 #continuously run game until game_over = true (when game ends)
 while not game_over:
-    # Get all events
-    gamedis.fill(black)
+    # Get all event
     score_change = False
     for event in pygame.event.get(): 
         '''
@@ -95,19 +95,21 @@ while not game_over:
             elif event.key == pygame.K_1:
                 score_change = True
                 game_score += 1
+                pygame.draw.rect(gamedis, orange, scoreboard)
                 score_display = score_font_type.render(f'Score: {game_score}', True, black) 
                 gamedis.blit(score_display, (30, 100))
                 pygame.display.update()
 
     #Did snake hit wall?
-    if pos_x > gamedis_width or pos_x < 0 or pos_y > gamedis_height or pos_y < 0:
-        game_over = True
-   
-
     pos_x += pos_x_change
     pos_y += pos_y_change      
- 
+
+    if pos_x > (gamedis_width + scoreboard_width) or pos_x < scoreboard_width or pos_y > gamedis_height or pos_y < 0:
+        game_over = True
+        break
+
     #Updating Snake Head
+    pygame.draw.rect(gamedis, black, game_area)
     gamedis.blit(snake_head, (pos_x, pos_y))
     pygame.display.update()
 
@@ -115,10 +117,11 @@ while not game_over:
 
 
 # You Lose XD
+pygame.draw.rect(gamedis, black,game_area)
 lose_text = score_font_type.render("RIP BOZO YOU LOSE", True, white)
-gamedis.blit(lose_text, (gamedis_width/2 + scoreboard_width / 2, gamedis_height/2))
+gamedis.blit(lose_text, (gamedis_width/2, gamedis_height/2))
 pygame.display.update()
-pygame.time.wait(3000)
+pygame.time.wait(2000)
 
 #Exiting the Game
 pygame.quit()
